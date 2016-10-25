71b8a7fa5c230e13b9c6e4794d11fba912307fd9
Dustin Chapman <dustin.r.chapman@gmail.com>
2016-10-25 01:55:24 -0400

    Second iteration of java word count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class WordCount {
-    private Map<String, Integer> wordCount;
 
     public Map<String, Integer> phrase(String given) {
-        for ( String word : given.split("\\W+") ) {
-            wordCount.merge(word.toLowerCase(), 1, Integer::sum);
-        }
+        List<String> elements = Arrays.asList(given.toLowerCase().split("\\W+"));
+        Map<String, Integer> wordCount = elements
+            .stream()
+            .collect(Collectors.groupingBy(s -> s, Collectors.summingInt(s -> 1)));
         return wordCount;
-    }
-
-    public WordCount() {
-        wordCount = new HashMap<String, Integer>();
     }
 }
